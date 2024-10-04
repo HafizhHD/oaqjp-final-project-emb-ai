727a52313398a138fdfe49eb8c9af49d7250c3e2
HafizhHD <17662247+HafizhHD@users.noreply.github.com>
2024-10-04 06:49:42 -0400

    fix: comment

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-    """Function printing python version."""
+    """Function printing python versionye."""
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myJson = { "raw_document": { "text": text_to_analyze } }
